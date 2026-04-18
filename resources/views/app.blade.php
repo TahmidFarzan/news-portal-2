3541041e37ec14659ee0c1b7a0e74e871479e685
Seikh Md Tahmid Farzan <tfarzan007@gmail.com>
2026-04-18 17:19:45 +0600

    Update for App Layout

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,9 +6,22 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="copyright" content="Copyright &copy; {{ now()->format('Y') }}. All Rights &reg; Reserved by {{ config("app.url") }}">
 
-        @routes
+        @if (request()->is('/'))
+            <meta http-equiv="refresh" content="300">
+        @endif
+
+        @if (!request()->is('back-office/*') && !request()->is('auth-user/*'))
+            <meta name="robots" content="index, follow">
+            <link rel="canonical" href="{{ request()->fullUrl() }}">
+        @endif
+
+        <title inertia>{{ config('app.name', 'News Portal 2') }}</title>
+
+        <link href="{{ config('app.app_favicon') }}" rel="icon" loading="lazy">
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        @routes
 
         @inertiaHead
     </head>
