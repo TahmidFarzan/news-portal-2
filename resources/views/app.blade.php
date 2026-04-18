b3d73beb1d7dbb4c906bd2dafe8884bfd6ebfdbb
Seikh Md Tahmid Farzan <tfarzan007@gmail.com>
2026-04-18 17:17:26 +0600

    Update for tightenco/ziggy

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,9 @@
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="copyright" content="Copyright &copy; {{ now()->format('Y') }}. All Rights &reg; Reserved by {{ config("app.url") }}">
+
+        @routes
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
