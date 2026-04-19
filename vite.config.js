53412738c0c9f0e3274b62dd6bc21a5bcbfc3710
Seikh Md Tahmid Farzan <tfarzan007@gmail.com>
2026-04-19 12:34:53 +0600

    Update for vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import Components from 'unplugin-vue-components/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
     plugins: [
@@ -8,11 +11,27 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+
+        vue(),
+
+        Components(),
+
         tailwindcss(),
     ],
+
     server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
+        host: '127.0.0.1',
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            host: '127.0.0.1',
         },
     },
-});
+
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources/js'),
+            ziggy: path.resolve('vendor/tightenco/ziggy/dist/vue.es'),
+        },
+    },
+})
