424946cfac9479409bab988be4fc3fdc60167e76
Seikh Md Tahmid Farzan <tfarzan007@gmail.com>
2026-04-19 15:57:17 +0600

    Update for dahsboard

---
+++ resources/js/pages/auth-user/Dashboard.vue
@@ -0,0 +1,29 @@
+<script setup>
+import Layout from '@/pages/layouts/AuthLayout.vue'
+import { onMounted, nextTick, inject } from 'vue'
+import { Head } from '@inertiajs/vue3'
+
+defineOptions({ layout: Layout })
+
+const pageReady = inject("pageReady")
+
+onMounted(async () => {
+    await nextTick()
+    window.dispatchEvent(new CustomEvent('set-breadcrumb', {
+        detail: [{ text: 'Dashboard', active: true }]
+    }))
+    pageReady.value = true
+})
+</script>
+
+<template>
+
+    <Head title="Dashboard" />
+
+    <div class="grid grid-cols-1 gap-4">
+
+        <div class="bg-white border border-gray-200 rounded-2xl p-6 shadow-sm">
+            Dashboard
+        </div>
+    </div>
+</template>
