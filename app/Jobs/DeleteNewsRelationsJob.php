83ac64b4189b3bd85285cc2accaf0ac8fdf558b2
Seikh Md Tahmid Farzan <tfarzan007@gmail.com>
2026-05-16 18:31:19 +0600

    DeleteNews Job update

--- app/Jobs/DeleteNewsRelationsJob.php
+++ app/Jobs/DeleteNewsRelationsJob.php
@@ -42,7 +42,7 @@ class DeleteNewsRelationsJob implements ShouldQueue, ShouldBeUnique
     {
         $news = News::find($this->newsId);
 
-        if ($news && ($news->activityLogs()->exists()) || ($news->getMedia($news->media_collection_name)->count() > 0)) {
+        if ($news && ($news->activityLogs()->exists()) || ($news->getMedia($news->media_collection_name)->count() > 0) || $news->tags()->exists() || $news->contributors()->exists() || $news->relevantNewses()->exists() || $news->relatedNewses()->exists()) {
             DB::beginTransaction();
             try {
 
@@ -52,6 +52,22 @@ class DeleteNewsRelationsJob implements ShouldQueue, ShouldBeUnique
 
                 if ($news->getMedia($news->media_collection_name)->count() > 0) {
                     $news->clearMediaCollection($news->media_collection_name);
+                }
+
+                if ($news->tags()->exists()) {
+                    $news->tags()->delete();
+                }
+
+                if ($news->contributors()->exists()) {
+                    $news->contributors()->delete();
+                }
+
+                if ($news->relevantNewses()->exists()) {
+                    $news->relevantNewses()->delete();
+                }
+
+                if ($news->relatedNewses()->exists()) {
+                    $news->relatedNewses()->delete();
                 }
 
                 DB::commit();
