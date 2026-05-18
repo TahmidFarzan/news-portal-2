ca56086a1c5c5f35972f8a66709eca40b886d872
Seikh Md Tahmid Farzan <tfarzan007@gmail.com>
2026-05-18 14:38:18 +0600

    Pagination index

--- resources/views/sitemaps/paginable-index.blade.php
+++ resources/views/sitemaps/paginable-index.blade.php
@@ -3,7 +3,7 @@
     @for ($i = 0; $i < $lastPage; $i++)
         <sitemap>
             <loc>{{ $routeUrl . '?page=' . ($i + 1) }}</loc>
-            <lastmod>{{ $i == 1 ? now()->format('Y-m-d\TH:i:sP') : now()->subWeeks(1)->format('Y-m-d\TH:i:sP') }}
+            <lastmod>{{ ($i + 1) == 1 ? now()->format('Y-m-d\TH:i:sP') : now()->subWeeks(1)->format('Y-m-d\TH:i:sP') }}
             </lastmod>
         </sitemap>
     @endfor
