31b511d3b068bfafdf686852649bd15b37086fd2
Seikh Md Tahmid Farzan <tfarzan007@gmail.com>
2026-06-06 12:44:07 +0600

    Update for News List and Grid

---
+++ resources/js/components/common/news/NewsGrid.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { computed } from 'vue'
+
+import GridCard from '@/Components/common/news/GridCard.vue'
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons'
+
+const { newsItems } = defineProps({
+    newsItems: {
+        type: Object,
+        required: true,
+    },
+})
+</script>
+
+<template>
+    <section v-if="newsItems.length" class="space-y-5">
+        <div class="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4">
+            <GridCard v-for="item in newsItems" :key="item?.id || item?.slug" :item="item" />
+        </div>
+    </section>
+</template>
---
+++ resources/js/components/common/news/NewsList.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { computed } from 'vue'
+
+import ListCard from '@/Components/common/news/ListCard.vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons'
+
+const {
+    newsItems,
+} = defineProps({
+    newsItems: {
+        type: Object,
+        required: true,
+    },
+})
+</script>
+
+<template>
+    <section v-if="newsItems.length" class="space-y-2 p-2">
+        <div class="grid grid-cols-1 lg:grid-cols-1 gap-3">
+            <ListCard v-for="(item, index) in newsItems" :key="item?.id || item?.slug || index" :item="item"/>
+        </div>
+    </section>
+</template>
