7af8aa75663345b7161370acc855cf9b020eb4f2
Seikh Md Tahmid Farzan <tfarzan007@gmail.com>
2026-06-08 12:22:35 +0600

    Update for MenuType,NewsType,UserRole

--- app/Models/UserRole.php
+++ app/Models/UserRole.php
@@ -1,23 +1,45 @@
 <?php
-
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Attributes\Table;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\Table;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 #[Table('user_roles')]
-#[Fillable(['name'])]
+#[Fillable(['name', 'slug'])]
 class UserRole extends Model
 {
-    use HasFactory;
+    use HasFactory, HasSlug;
 
     protected function casts(): array
     {
         return [
-            'created_at'        => 'datetime',
-            'updated_at'        => 'datetime',
+            'created_at' => 'datetime',
+            'updated_at' => 'datetime',
         ];
+    }
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->saveSlugsTo('slug')
+            ->generateSlugsFrom(function ($model) {
+                $mainSlug     = $model->name;
+                $randomString = Str::random(11);
+                $createdAt    = $model->created_at ?? now();
+                $createdAt    = $createdAt->format('HisdmY');
+                return "{$mainSlug}-{$randomString}-{$createdAt}";
+            })
+            ->doNotGenerateSlugsOnUpdate()
+            ->slugsShouldBeNoLongerThan(255);
+    }
+
+    public function getRouteKeyName(): string
+    {
+        return 'slug';
     }
 }
