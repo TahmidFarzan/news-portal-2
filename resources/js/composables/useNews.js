b773837779e0d917e8f0fa97ae6d398acbce873b
Seikh Md Tahmid Farzan <tfarzan007@gmail.com>
2026-06-18 14:12:39 +0600

    Update for useNews Composable

--- resources/js/composables/useNews.js
+++ resources/js/composables/useNews.js
@@ -1,21 +1,37 @@
 export const newsTypes = {
-    Story: 'Story',
-    Video: 'Video',
-    ImageGallery: "Image Gallery",
+    Story: {
+        Name: 'Story',
+        Slug: 'story',
+    },
+    Video: {
+        Name: 'Video',
+        Slug: 'video',
+    },
+    ImageGallery: {
+        Name: 'Image Gallery',
+        Slug: 'image-gallery',
+    },
 }
 
 export const getNewsTypeName = (newsType) => {
     return newsType?.name?.toLowerCase() ?? ''
 }
 
+export const getNewsTypeSlug = (newsType) => {
+    return newsType?.slug?.toLowerCase() ?? ''
+}
+
 export const isStory = (newsType) => {
-    return getNewsTypeName(newsType) === newsTypes.Story.toLowerCase()
+    return getNewsTypeName(newsType) === newsTypes.Story.Name.toLowerCase()
+        || getNewsTypeSlug(newsType) === newsTypes.Story.Slug
 }
 
 export const isVideo = (newsType) => {
-    return getNewsTypeName(newsType) === newsTypes.Video.toLowerCase()
+    return getNewsTypeName(newsType) === newsTypes.Video.Name.toLowerCase()
+        || getNewsTypeSlug(newsType) === newsTypes.Video.Slug
 }
 
 export const isImageGallery = (newsType) => {
-    return getNewsTypeName(newsType) === newsTypes.ImageGallery.toLowerCase()
+    return getNewsTypeName(newsType) === newsTypes.ImageGallery.Name.toLowerCase()
+        || getNewsTypeSlug(newsType) === newsTypes.ImageGallery.Slug
 }
