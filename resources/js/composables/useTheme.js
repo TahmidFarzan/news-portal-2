de65cd684ff116bdffcc1cfa1ae1577143061619
Seikh Md Tahmid Farzan <tfarzan007@gmail.com>
2026-08-10 10:55:08 +0600

    Google ad sence

--- resources/js/composables/useTheme.js
+++ resources/js/composables/useTheme.js
@@ -35,7 +35,7 @@ export const themeOptions = Object.freeze({
     GOOGLE_ANALYTIC_HEADER  : "Google Analytic Header",
     GOOGLE_TAG_MANAGER_HEADER  : "Google Tag Manager Header",
     GOOGLE_TAG_MANAGER_BODY  : "Google Tag Manager Body",
-    GOOGLE_ADSENSE_CLIENT_ID: "Google GoogleAdsense Client Id"
+    GOOGLE_ADSENSE_CLIENT_ID: "Google Google Adsense Client Id"
 })
 
 export function useTheme() {
